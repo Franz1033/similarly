4346f88eb723edd8cbbc1cdf3f96538ce0d6367c
Franz1033 <franzzapanta1033@gmail.com>
2023-02-02 23:36:27 +0800

    designed homepage

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,47 @@
+from django.http import Http404, HttpResponseServerError, HttpResponseRedirect
 from django.shortcuts import render
+from json import JSONDecodeError
+from django.urls import reverse
 import requests
 import json
 
 '''
+TasteDive sample API endpoint:
+url = "https://tastedive.com/api/similar?q=red+hot+chili+peppers%2C+pulp+fiction"
+
 See documentation on: https://tastedive.com/read/api
 '''
 
-# TasteDive API endpoint
-url = "https://tastedive.com/api/similar?q=red+hot+chili+peppers%2C+pulp+fiction"
-
-# Obtain the API data
-data = requests.get(url)
-
-# retrieve results using json.loads() function
-results = json.loads(data.text)
+api_key = "447490-Similarl-CZJEL2ZA"
 
 def index(request):
-    return render(request, "main_app/index.html", {
-        "results": results
-    })
+    return render(request, "main_app/index.html")
+
+def search(request):
+    if request.method == "GET":
+
+        query = request.GET.get("query").lower()
+        category = request.GET.get("category").lower()
+
+        # Obtain API endpoint data
+        data = requests.get(f"https://tastedive.com/api/similar?q={query}&type={category}&info=1&k={api_key}")
+
+        if category == "any":
+            data = requests.get(f"https://tastedive.com/api/similar?q={query}&info=1&k={api_key}")
+
+        try:
+
+            # Retrieve results 
+            results = json.loads(data.text)
+        
+        except JSONDecodeError:
+            raise HttpResponseServerError()
+
+        return render(request, "main_app/search.html", {
+            "results": results
+        })
+
+    else:
+        return HttpResponseRedirect(reverse("index"))    
+
+
